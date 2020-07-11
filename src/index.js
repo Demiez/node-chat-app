de2389ad5b35b01c4fcbaff00e6596f994bc0320
Dmytro Petrenko <dmg.petrenko@gmail.com>
2020-07-11 12:42:50 +0300

    broadcasting events on join and leaving chat

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const socketio = require('socket.io')
 
 const app = express()
 const server = http.createServer(app)
-const io = socketio(server)d
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirPath = path.join(__dirname, '../public')
@@ -15,10 +15,16 @@ app.use(express.static(publicDirPath))
 //io.on runs according to the number of connections
 io.on('connection', (socket) => {
     console.log('New Websocket connection')
+    // broadcast emits event to all except this particular socket
+    socket.broadcast.emit('message', 'A new user has joined!')
     
     socket.emit('message', 'Welcome!')
     socket.on('sendMessage', (message) => {
         io.emit('message', message)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'User has left')
     })
 })
 
